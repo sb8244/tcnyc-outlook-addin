6a824dd42f8f407be6315c65a96315e0f35007cc
Stephen Bussey <steve.bussey@salesloft.com>
2016-05-08 08:19:32 -0400

    make company url be better for andrew

--- appread/home/home.controller.js
+++ appread/home/home.controller.js
@@ -13,17 +13,23 @@
 
     vm.email_address = Office.context.mailbox.item.from.emailAddress;
     companyApi.findForEmail(vm.email_address).then(function(resp) {
-      vm.company_html = resp.data.company_html;
+      vm.company_html = enrichCompanyHtml(resp.data.company_html);
       vm.user_html = resp.data.html;
     }).catch(function(err) {
       return companyApi.findCompanyForEmail(vm.email_address).then(function(resp) {
-        vm.company_html = resp.data.company_html;
+        vm.company_html = enrichCompanyHtml(resp.data.company_html);
       }).catch(function(err) {
         vm.not_found = true;
       });
     }).finally(function() {
       vm.loading = false;
     });
+  }
+
+  function enrichCompanyHtml(html) {
+    var el = angular.element(html);
+    el.find("h3").prepend(angular.element("<i class='ms-Icon ms-Icon--work'>"))
+    return el.html();
   }
 
 })();
